ee88b806b804ad0c5e316c635c5ee040dbf05c21
tomoki <tomoki10wakhok@gmail.com>
2015-06-18 22:19:56 +0900

    変数名の修正

--- bayes_coin_r2.py
+++ bayes_coin_r2.py
@@ -3,26 +3,26 @@ range_first=0   #range
 range_last=1
 theta=Symbol('theta')
 k=Symbol('k')
-beforeP = 1
+priorD = 1 #prior probability
 posORnega = 0
 count = 0
 
 while True:
     count+=1
     print
-    print "Negative:0   Positive:1 please input number:"
+    print "Negative:0   Positive:1 please input a number=>"
     posORnega = input()
-    print "Before Production = ",beforeP
+    print "Prior Distribution = ",priorD
     if posORnega == 1:
         thetaT = theta
     elif posORnega == 0:
         thetaT = 1-theta
     
-    eq = Eq(1,k*integrate(beforeP*thetaT, (theta, range_first, range_last)))
+    eq = Eq(1,k*integrate(priorD*thetaT, (theta, range_first, range_last)))
     print eq
     solList=solve(eq, k)
     print "k =",solList[0]
-    result = solList[0]*beforeP*thetaT
-    print "pi( theta| D",count,") = ", result
-    print "After Production ",thetaT," = ",integrate(result*thetaT,(theta,range_first, range_last))
-    beforeP = result
+    result = solList[0]*priorD*thetaT
+    print "Posterior Distribution pi( theta| D",count,") = ", result
+    print "Mean ",thetaT," = ",integrate(result*thetaT,(theta,range_first, range_last))
+    priorD = result
