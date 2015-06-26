94f30de8e088b97e0ba59618e9d9c1a350e1129b
tomoki <tomoki10wakhok@gmail.com>
2015-06-26 15:29:48 +0900

    文字コード変更

--- bayes_coin_r2.py
+++ bayes_coin_r2.py
@@ -1,28 +1,31 @@
+#-*- coding:utf-8 -*-#
 from sympy import *
-range_first=0   #range
-range_last=1
 theta=Symbol('theta')
 k=Symbol('k')
-priorD = 1 #prior probability
-posORnega = 0
+beforeP = 1         #事前分布の平均
+headORtail = 0    #表か裏か
 count = 0
 
 while True:
     count+=1
     print
-    print "Negative:0   Positive:1 please input a number=>"
-    posORnega = input()
-    print "Prior Distribution = ",priorD
-    if posORnega == 1:
+    print "Negative:0   Positive:1 please input a number:"
+    headORtail = input()
+    print "Before Production theta mean= ",beforeP
+    if headORtail == 1:
         thetaT = theta
-    elif posORnega == 0:
+    elif headORtail == 0:
         thetaT = 1-theta
     
-    eq = Eq(1,k*integrate(priorD*thetaT, (theta, range_first, range_last)))
+    #1=k(尤度(thetaT)*事前分布(beforeP)
+    eq = Eq(1,k*integrate(beforeP*thetaT, (theta, 0, 1)))       #規格化の条件でkを求める
     print eq
-    solList=solve(eq, k)
-    print "k =",solList[0]
-    result = solList[0]*priorD*thetaT
-    print "Posterior Distribution pi( theta| D",count,") = ", result
-    print "Mean ",thetaT," = ",integrate(result*thetaT,(theta,range_first, range_last))
-    priorD = result
+    print "k =",solve(eq, k)
+    result = solve(eq, k)[0]*beforeP*thetaT
+    print "pi( theta| D",count,") = ", result
+    if headORtail == 1:
+        print "After Production theta mean = ",integrate(result*thetaT,(theta,0, 1))
+    elif headORtail == 0:
+        print "After Production theta mean = ",1-integrate(result*thetaT,(theta,0, 1))
+    beforeP = result
+    plot(result,(theta,-2.0,2.0),axis_center='center')
